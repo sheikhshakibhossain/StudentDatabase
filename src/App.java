d57eb21f72c5b5746b3616b959bad4465c963711
sheikhshakibhosaain <sheikhshakibpro@gmail.com>
2022-11-29 18:16:33 +0600

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import admission.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Student st = new Student();
+        st.info();
     }
 }
